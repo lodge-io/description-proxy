f2cb759b77e6bbfb190022452808a0f0d2a6a1ea
SimonC989 <ssimon-chan989@gmail.com>
2019-04-10 23:10:21 -0700

    proxies all modules correctly

--- server/server.js
+++ server/server.js
@@ -1,10 +1,57 @@
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
 
 const app = express();
 const port = 4000;
 
-app.use(express.static(path.join(__dirname, '../client')))
+app.use('/listing/:listingid', express.static(path.join(__dirname, '../client')))
+
 app.listen(port, () => {
   console.log('Running on port:', port);
+});
+
+const nav = axios.create({
+  baseURL: 'http://localhost:3223'
+});
+
+const description = axios.create({
+  baseURL: 'http://localhost:3116'
+});
+
+const gallery = axios.create({
+  baseURL: 'http://localhost:8000'
+});
+
+const review = axios.create({
+  baseURL: 'http://localhost:3000'
+});
+
+
+app.get('/api/location/:query', (req, res) => {
+  var query = req.params.query;
+  nav.get(`/api/location/${query}`)
+    .then(response => res.json(response.data))
+    .catch(err => res.send(err))
+});
+
+app.get('/api/host/:hostid', (req, res) => {
+  var id = req.params.hostid;
+  description.get(`/api/host/${id}`)
+    .then(response => res.json(response.data))
+    .catch(err => res.send(err))
+});
+
+app.get('/rooms/:id/', (req, res) => {
+  var id = req.params.id;
+  gallery.get(`/rooms/${id}`)
+    .then(response => res.json(response.data))
+    .catch(err => res.send(err))
+});
+
+app.get('/reviews/:id', (req, res) => {
+  var id = req.params.id;
+  review.get(`/reviews/${id}`)
+    .then(response => res.json(response.data))
+    .catch(err => res.send(err))
 });
